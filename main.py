4f0c9ef398e1d11c2cdcc96d429957f1052149b8
Ed Haslam <ed@users.noreply.github.com>
2020-03-28 19:28:01 +1300

    add collision detection

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import pygame as pg
+from collections import OrderedDict
+
+def add_avatar_tracks(objects):
+    a_coords,a_image = objects['avatar']
+    bg_coords,bg_image = objects['bg']
+
+    bg.blit(track,a_coords)
+
+    return (bg_coords,bg_image)
 
 def get_avatar(objects, speed=2):
     (a_x, a_y),a_image = objects['avatar']
@@ -16,7 +25,21 @@ def get_avatar(objects, speed=2):
     else:
         new_a_x = a_x
         new_a_y = a_y
-    
+
+    if 'obstacle' in objects:
+        (o_x,o_y),o_image = objects['obstacle']
+        o_w,o_h = o_image.get_width(),o_image.get_height()
+        a_w,a_h = a_image.get_width(),a_image.get_height()
+
+        a_points = [(new_a_x,new_a_y),(new_a_x+a_w,new_a_y),
+                    (new_a_x,new_a_y+a_h),(new_a_x+a_w,new_a_y+a_h)]
+
+        for x,y in a_points:
+            if o_x<x<o_x+o_w and o_y<y<o_y+o_h:
+                new_a_x = a_x
+                new_a_y = a_y
+                break
+                
     return ((new_a_x,new_a_y),a_image)
 
 width,height = 800,600
@@ -25,13 +48,17 @@ exit_event_types = [pg.QUIT]
 
 avatar = pg.image.load('images/avatar.png')
 target = pg.image.load('images/target.png')
+obstacle = pg.image.load('images/obstacle.png')
+track = pg.image.load('images/track.png')
+bg = pg.Surface((width,height))
+bg.fill(pg.color.THECOLORS['white'])
 
 pg.init()
 
 display = pg.display.set_mode((width,height))
 clock = pg.time.Clock()
 
-objects = {'avatar':((1,1),avatar)}
+objects = OrderedDict({'bg':((0,0),bg), 'avatar':((1,1),avatar), 'obstacle':((400,300),obstacle)})
 
 exit_game = False
 
@@ -44,8 +71,7 @@ while not exit_game:
             objects['target'] = ((mouse_x,mouse_y),target)
 
     objects['avatar'] = get_avatar(objects)
-
-    display.fill(pg.color.THECOLORS['white'])
+    objects['bg'] = add_avatar_tracks(objects)
 
     for _,((x,y),image) in objects.items():
         display.blit(image,(round(x),round(y)))
