5f07060b46d9ff259bce7e7cb00e394600e5daf0
Ed Haslam <ed@users.noreply.github.com>
2020-03-28 18:14:12 +1300

    added basic pathfinding

--- main.py
+++ main.py
@@ -1,37 +1,54 @@
 import pygame as pg
 
-def render_avatar(x,y):
-    display.blit(avatar, (x,y))
+def get_avatar(objects, speed=2):
+    (a_x, a_y),a_image = objects['avatar']
+
+    if 'target' in objects:
+        (t_x, t_y),_ = objects['target']
+        hyp_len = ((t_x-a_x)**2 + (t_y-a_y)**2)**0.5
+
+        if hyp_len<1:
+            new_a_x = a_x
+            new_a_y = a_y
+        else:
+            new_a_x = a_x + (t_x-a_x)/hyp_len*speed
+            new_a_y = a_y + (t_y-a_y)/hyp_len*speed
+    else:
+        new_a_x = a_x
+        new_a_y = a_y
+    
+    return ((new_a_x,new_a_y),a_image)
 
 width,height = 800,600
+
+exit_event_types = [pg.QUIT]
+
 avatar = pg.image.load('images/avatar.png')
+target = pg.image.load('images/target.png')
 
 pg.init()
 
 display = pg.display.set_mode((width,height))
 clock = pg.time.Clock()
 
-valid_exit_events = [pg.QUIT]
+objects = {'avatar':((1,1),avatar)}
 
-x,y = 1,1
-xinc,yinc = 1,1
+exit_game = False
 
-while True:
-    exit_events = [True for event in pg.event.get() if event in valid_exit_events]
+while not exit_game:
+    for event in pg.event.get():
+        if event.type in exit_event_types:
+            exit_game = True
+        if event.type == pg.MOUSEBUTTONUP:
+            mouse_x,mouse_y = pg.mouse.get_pos()
+            objects['target'] = ((mouse_x,mouse_y),target)
 
-    if len(exit_events) > 0:
-        break
-
-    if not 0<x<width:
-        xinc *= -1
-    if not 0<y<height:
-        yinc *= -1
-    
-    x += xinc
-    y += yinc
+    objects['avatar'] = get_avatar(objects)
 
     display.fill(pg.color.THECOLORS['white'])
-    render_avatar(x,y)
+
+    for _,((x,y),image) in objects.items():
+        display.blit(image,(round(x),round(y)))
     
     pg.display.update()
     clock.tick(60)    
